f171719ae8e163b5970b792edcf7f9c4b13ca99a
VictorWebDew <frontendmy@gmail.com>
2022-11-04 00:42:19 +0200

    add correction

--- JS_Home__Work_1/scrypt.js
+++ JS_Home__Work_1/scrypt.js
@@ -2,21 +2,31 @@
 // -Вивести кожну змінну за допомогою: console.log
 let hello, owu, com, ua, one, ten, minus, oneTwoThree, three_14, two_7, sixteen, pureTrue, pureFalse;
 hello = "hello";
+console.log(hello);
 owu = "owu";
+console.log(owu);
 com = "com";
+console.log(com);
 ua = "ua";
+console.log(ua);
 one = 1;
+console.log(one);
 ten = 10;
+console.log(ten);
 minus = "-999";
+console.log(minus);
 oneTwoThree = 123;
+console.log(oneTwoThree);
 three_14 = 3.14;
+console.log(three_14);
 two_7 = 2.7;
+console.log(two_7);
 sixteen = 16;
+console.log(sixteen);
 pureTrue = true;
+console.log(pureTrue);
 pureFalse = false;
-let allTogethre = `${hello}, ${owu}, ${com}, ${ua}, ${one}, ${ten}, ${minus}, ${oneTwoThree}, ${three_14}, ${two_7}, ${sixteen}, ${pureTrue}, ${pureFalse};`
-console.log(allTogethre);
-
+console.log(pureFalse);
 
 // Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
 
@@ -24,7 +34,7 @@ let firstName = "Victor";
 let middleName = "Victorovich";
 let lastName = "Sichkov";
 
-// const person = "firstName" + "" + "middleName" + "" + "lastName";
+// const person = firstName + " " + middleName + " " + lastName;
 
 const person = `${firstName} ${middleName} ${lastName}`;
 console.log(person);
